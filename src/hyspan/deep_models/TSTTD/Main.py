7c023842914afa2473fec891c94ca6bab1ed7121
michaelpiro <michael.pirogovsky@mail.huji.ac.il>
2026-03-06 18:19:48 +0200

    change to relative imports

--- src/hyspan/deep_models/TSTTD/Main.py
+++ src/hyspan/deep_models/TSTTD/Main.py
@@ -2,7 +2,7 @@ import yaml
 from .Train_eval import train, eval_model, select_best
 import os
 current_dir = os.path.dirname(__file__)
-config_file_path = os.path.join(current_dir, "config.yaml")
+config_file_path = os.path.join(current_dir, "deep_models/TSTTD/config.yaml")
 def main(config_path=config_file_path):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
